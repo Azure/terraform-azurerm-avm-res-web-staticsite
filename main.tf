ac5ac1da6ac677ebd23319d7d56f4bffeafdfa8b
donovm4 <mccoy.donov@gmail.com>
2023-12-18 11:34:37 -0500

    implement azapi_update_resource

--- main.tf
+++ main.tf
@@ -9,11 +9,63 @@ resource "azurerm_static_site" "this" {
     for_each = var.identities
 
     content {
-      type         = identities.identity_type
-      identity_ids = identites.identity_ids
+      type         = identity.value.identity_type
+      identity_ids = identity.value.identity_ids
     }
   }
 
   app_settings = var.app_settings
   tags         = var.tags
 }
+
+resource "azapi_update_resource" "example" {
+  count = var.repositoryUrl != null ? 1 : 0
+  
+  type = "Microsoft.Web/staticSites@2022-03-01"
+  resource_id = azurerm_static_site.this.id
+  body = jsonencode({
+    properties = {
+      repositoryUrl = var.repositoryUrl
+      branch = var.repositoryUrl != null ? coalesce(var.branch, "main") : null
+    }
+  })
+  depends_on = [ azurerm_static_site.this ]
+}
+
+# Custom Domains not yet currently through AVM module
+
+# resource "azurerm_static_site_custom_domain" "this" {
+#   for_each = var.custom_domains
+
+#   static_site_id = azurerm_static_site.this.id
+#   domain_name = coalesce(each.value.domain_name, "${each.value.cname_name}.${each.value.cname_zone_name}")
+#   validation_type = each.value.validation_type  
+# }
+
+# resource "azurerm_dns_cname_record" "this" {
+#   for_each = var.custom_domains
+
+#   name = each.value.cname_name
+#   zone_name = each.value.cname_zone_name
+#   resource_group_name = coalesce(each.value.resource_group_name, var.resource_group_name)
+#   ttl = each.value.ttl
+#   record = each.value.cname_record
+#   target_resource_id = each.value.cname_target_resource_id
+# }
+
+# resource "azurerm_dns_txt_record" "this" {
+#   for_each = var.custom_domains
+
+#   name = each.value.txt_name
+#   zone_name = each.value.txt_zone_name
+#   resource_group_name = coalesce(each.value.resource_group_name, var.resource_group_name)
+#   ttl = each.value.ttl
+  
+#   dynamic "record" {
+#     for_each = each.value.txt_records
+
+#     content {
+#       value = record.value.value
+#     }
+#   }
+# }
