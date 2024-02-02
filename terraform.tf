2473f9feb5b2f2b1438faf2e0cafb891c726a5d5
donovm4 <mccoy.donov@gmail.com>
2024-02-02 14:48:39 -0500

    order required providers

--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = ">= 1.5.2"
   required_providers {
     # TODO: Ensure all required providers are listed here.
+    azapi = {
+      source  = "azure/azapi"
+      version = ">= 0.1.0, < 1.10.0"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.71.0, < 4.0.0"
@@ -9,10 +13,6 @@ terraform {
     random = {
       source  = "hashicorp/random"
       version = ">= 3.5.0, < 4.0.0"
-    }
-    azapi = {
-      source  = "azure/azapi"
-      version = ">= 0.1.0, < 1.10.0"
     }
   }
 }
