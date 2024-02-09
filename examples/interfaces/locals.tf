7378eda39a42be1c8e826f74dd09b3814165b783
Donovan McCoy <144926888+donovm4@users.noreply.github.com>
2024-02-09 15:47:20 -0500

    fixes for proper private dns zone naming and private dns zone vnet li… (#6)
    * fixes for proper private dns zone naming and private dns zone vnet linking
    
    * bug fix for region/capacity selections
    
    * linting fix to sku selector functionality for vm, addition to outputs
    
    * running ./avm pre-commit
    
    * local.default_host_name to reference resource_uri
    
    * updating dns check to use resource_private_endpoints

--- examples/interfaces/locals.tf
+++ examples/interfaces/locals.tf
@@ -7,4 +7,8 @@ locals {
     "westeurope",
     "eastasia"
   ]
+  azurerm_private_dns_zone_resource_name = "privatelink.${local.reformatted_subdomain}"
+  default_host_name                      = module.staticsite.resource_uri
+  reformatted_subdomain                  = join(".", slice(local.split_subdomain, 1, length(local.split_subdomain)))
+  split_subdomain                        = split(".", local.default_host_name)
 }
