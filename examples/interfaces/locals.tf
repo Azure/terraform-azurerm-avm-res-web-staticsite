5c7626d741b85508f57de273485676c0e173c35e
donovm4 <mccoy.donov@gmail.com>
2024-02-09 14:11:19 -0500

    linting fix to sku selector functionality for vm, addition to outputs

--- examples/interfaces/locals.tf
+++ examples/interfaces/locals.tf
@@ -7,6 +7,7 @@ locals {
     "westeurope",
     "eastasia"
   ]
+  
   azurerm_private_dns_zone_resource_name = "privatelink.${local.reformatted_subdomain}"
   default_host_name                      = module.staticsite.azurerm_static_site.default_host_name
   reformatted_subdomain                  = join(".", slice(local.split_subdomain, 1, length(local.split_subdomain)))
--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,27 @@
 # Module owners should include the full resource via a 'resource' output
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
-output "azurerm_static_site" {
+
+output "name" {
+  description = "The name of the static site."
+  value       = azurerm_static_site.this.name
+}
+
+output "resource" {
   description = "This is the full output for the resource."
   value       = azurerm_static_site.this
 }
 
-output "private_endpoints" {
+output "resource_id" {
+  description = "The ID of the static site."
+  value       = azurerm_static_site.this.id
+}
+
+output "resource_private_endpoints" {
   description = "A map of private endpoints. The map key is the supplied input to var.private_endpoints. The map value is the entire azurerm_private_endpoint resource."
   value       = azurerm_private_endpoint.this
 }
 
-output "uri" {
+output "resource_uri" {
   description = "The default hostname of the static site."
   value       = azurerm_static_site.this.default_host_name
 }
